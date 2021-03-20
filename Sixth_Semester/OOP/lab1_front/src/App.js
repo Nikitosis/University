263d7913110ca0701d642e2c9c3426ed833d4543
Nikitosis <nikitospupov@gmail.com>
2021-03-20 11:08:12 +0200

    Added navigation

--- Sixth_Semester/OOP/lab1_front/src/App.js
+++ Sixth_Semester/OOP/lab1_front/src/App.js
@@ -3,15 +3,18 @@ import React from "react";
 import Navbar from "./navbar/Navbar";
 import CardsPage from "./content/CardsPage";
 import styles from "./App.module.css";
+import {BrowserRouter, Route} from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="App container-fluid h-100 p-0">
-        <Navbar/>
-        <div className={` p-0 min-vh-100`}>
-            <CardsPage/>
+      <BrowserRouter>
+        <div className="App container-fluid h-100 p-0">
+            <Navbar/>
+            <div className={`p-0 min-vh-100`}>
+                <Route exact path={"/my-cards"} component={CardsPage}/>
+            </div>
         </div>
-    </div>
+      </BrowserRouter>
   );
 }
 
--- Sixth_Semester/OOP/lab1_front/src/navbar/Navbar.jsx
+++ Sixth_Semester/OOP/lab1_front/src/navbar/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import styles from "./Navbar.module.css";
+import {NavLink} from "react-router-dom";
 
 const Navbar = (props) => {
     return (
         <nav className="navbar navbar-expand-md navbar-dark bg-primary">
             <ul className="nav">
                 <li className="nav-item">
-                    <a className={`nav-link ${styles.nav_link}`} href="#">My cards</a>
+                    <NavLink className={`nav-link ${styles.nav_link}`} to="/my-cards" activeClassName={styles.nav_link__active}>My cards</NavLink>
                 </li>
                 <li className="nav-item">
-                    <a className={`nav-link ${styles.nav_link}`} href="#">Users</a>
+                    <NavLink className={`nav-link ${styles.nav_link}`} to="/users" activeClassName={styles.nav_link__active}>Users</NavLink>
                 </li>
             </ul>
         </nav>
