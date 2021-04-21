906afc1e531b04a01436e3e16e91b18981fee5c8
Nikitosis <nikitospupov@gmail.com>
2021-04-21 16:14:01 +0300

    Added test

---
+++ Sixth_Semester/OOP/Lab1/src/main/java/repository/ConnectionPool.java
@@ -0,0 +1,62 @@
+package repository;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConnectionPool {
+    public static ConnectionPool INSTANCE = new ConnectionPool();
+
+    private final List<Connection> connectionPool;
+    private final List<Connection> usedConnections = new ArrayList<>();
+    private static final int INITIAL_POOL_SIZE = 2;
+    private static final int MAX_POOL_SIZE = 5;
+
+    private ConnectionPool() {
+        connectionPool = new ArrayList<>(INITIAL_POOL_SIZE);
+        for (int i = 0; i < INITIAL_POOL_SIZE; i++) {
+            connectionPool.add(createConnection());
+        }
+    }
+
+    public Connection getConnection() {
+        if (connectionPool.isEmpty()) {
+            if (usedConnections.size() < MAX_POOL_SIZE) {
+                connectionPool.add(createConnection());
+            } else {
+                throw new RuntimeException("Maximum pool size reached, no available connections!");
+            }
+        }
+
+        Connection connection = connectionPool.remove(connectionPool.size() - 1);
+
+        usedConnections.add(connection);
+        return connection;
+    }
+
+    public boolean releaseConnection(Connection connection) {
+        connectionPool.add(connection);
+        return usedConnections.remove(connection);
+    }
+
+    private static Connection createConnection() {
+        return ConnectionFactory.getConnection();
+    }
+
+    public int getSize() {
+        return connectionPool.size() + usedConnections.size();
+    }
+
+    public List<Connection> getConnectionPool() {
+        return connectionPool;
+    }
+
+    public void shutdown() throws SQLException {
+        usedConnections.forEach(this::releaseConnection);
+        for (Connection c : connectionPool) {
+            c.close();
+        }
+        connectionPool.clear();
+    }
+}
