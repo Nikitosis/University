c2c9b77515bc49edb5c2131e7fcd7f5b6004346a
Nikitosis <nikitospupov@gmail.com>
2021-06-03 19:56:56 +0300

    Fixes

---
+++ Sixth_Semester/Криптология/lab2/src/test/java/RC6Test.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RC6Test {
+
+    @Test
+    void run() {
+        RC6 a = new RC6();
+        a.run();
+        Assert.assertEquals(a.text, a.decrypted_text);
+    }
+}
